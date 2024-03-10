1d5977bfedcfb2346217478f067b8321cd7fe707
HEX9CF <hex9cf@aliyun.com>
2024-03-10 12:15:00 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+const { Menu } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: path.join(__dirname, './public/logo.png'),
+    })
+    Menu.setApplicationMenu(null);
+    win.loadFile('index.html')
+}
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
